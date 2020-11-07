86f8914cbfb1866a292f1b6c2c2f4f9cdcbd65f0
Nickolas Menjibar <nickmenjibar@gmail.com>
2020-11-06 17:08:24 -0800

    Python File

---
+++ CSE-155-Photo-Translator/src/translate.py
@@ -0,0 +1,36 @@
+# Image text translator - see more in readme.
+# Made by @TomasRoj in 2019 under MIT license.
+# Imports all required components
+from PIL import Image
+import pytesseract
+from googletrans import Translator
+import os
+import glob
+
+# Sets up the googletranslator api
+translator = Translator()
+
+# Edit this path to tesseract.exe on your system. On windows its by default in the following:
+pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
+
+# Path to the images folder. Edit this to yours.
+os.chdir("C:\Users\nickm\Documents\GitHub\CSE-155\CSE-155-Photo-Translator\Image")
+
+# Goes thru every image:
+for images in glob.glob("*.jpg"):
+    # Opens image
+    im = Image.open(images)
+
+    # Gets the image and translates it to Czech. You can specify own lang if you want.
+    text = pytesseract.image_to_string(im, lang = 'eng')
+    text_translated = translator.translate(text, dest="cs")
+
+    # Finally, print the translated image's text and print it.
+
+    text_file = open("Output.txt", "w")
+
+    text_file.write("Purchase Amount: " 'TotalAmount')
+    print(text_translated)
+    print("\n")
+
+# Again and again.
--- CSE-155-Photo-Translator/templates/index.js
+++ CSE-155-Photo-Translator/templates/index.js
@@ -7,22 +7,10 @@ $(function () {
             reader.readAsDataURL(this.files[0]);
         }
     });
+
+    $("translate").change(function () {
+        
+    });
   });
 
-function readURL(input) {
-    if (input.files && input.files[0]) {
-        var reader = new FileReader();
 
-        reader.onload = function (e) {
-            $('#myImg').attr('src', e.target.result)
-            //.width(500)
-            //.height(200);
-    }
-
-    reader.readAsDataURL(input.files[0]);
-    }
-}
-
-$("#imgInp").change(function() {
-  readURL(this);
-});
